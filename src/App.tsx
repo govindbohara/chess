68cc86f28029a618e526259d7d64d4e4855275d4
govindbohara <govindbohara@cagtu.com>
2022-11-09 17:17:38 +0545

    added chess

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,80 @@
-import { useEffect, useState } from "react";
-
 
 export default function App() {
  
  let counter = 0;
 
   const possibleSteps: number[][] = [
-    [3, 1],
-    [1,3],
-    [-1,3],
-    [3, -1],
-    [-3, 1],
-    [-3, -1],
-    [1,-3],
-    [-1,-3]
+    [2, 1],
+    [1,2],
+    [-1,2],
+    [2, -1],
+    [-2, 1],
+    [-2, -1],
+    [1,-2],
+    [-1,-2]
     
   ];
+  
+  const getShortestDistance = (Array: number[][], finish: number[]) :number[] => {
+    const distanceFromFinish = Array.map((points) => {
+      return Math.sqrt(Math.pow(finish[0] - points[0], 2) + Math.pow(finish[1] - points[1], 2));
+    })
 
+    const shortestDistance = Math.min(...distanceFromFinish);
+    console.log(distanceFromFinish);
 
-  const getShortestDistance = (Array: number[][], finish: number[]) => {
-    counter++;
-    
-    const distance = Array.map((points) => {
-      return Math.sqrt(
-         Math.pow(points[1] - points[0], 2) +Math.pow(finish[1] - finish[0], 2)
-      );
-    });
-    const minNum = Math.min(...distance);
-    console.log(distance, "minnum:",minNum);
-    
-    const index = distance.indexOf(minNum);
-    return Array[index];
     
     
-    
+    const index = distanceFromFinish.indexOf(shortestDistance);
+    const finalCoordinates = Array[index]
    
+    return finalCoordinates;
+  
   };
+  
+ 
 
   const getPossibleCoordinates = (
     currentPosition: number[],
     targetPosition: number[]
   ) => {
-   
-    const allSteps = possibleSteps?.map((steps) => {
-      return [currentPosition[0] + steps[0], currentPosition[1] + steps[1]];
-    });
-   
-    const reducedArray = allSteps?.filter((steps) => (
-      steps[0] >= 0 && steps[0] <= 7 && steps[1] >= 0 && steps[1] <= 7
-    ))
+    if(JSON.stringify(currentPosition) !== JSON.stringify(targetPosition)){
+      
+      counter ++;
+      const allSteps = possibleSteps?.map((steps) => {
+        return [currentPosition[0] + steps[0], currentPosition[1] + steps[1]];
+      });
+     
+      const reducedArray = allSteps?.filter((steps) => (
+        steps[0] >= 1 && steps[0] <= 8 && steps[1] >= 1 && steps[1] <= 8
+      ))
+
+      console.log(reducedArray);
+      const currentCoordinate =getShortestDistance(reducedArray, targetPosition);
+      console.log(currentCoordinate);
+      
+      if(JSON.stringify(currentCoordinate) !== JSON.stringify(targetPosition)){
+       
+        getPossibleCoordinates(currentCoordinate,targetPosition)
+      }
+      
+
+      if(JSON.stringify(currentCoordinate) === JSON.stringify(targetPosition)){
+      alert("You have reached the target in " + counter + " steps");
+      }
+     
     
-    const currentCoordinate =getShortestDistance(reducedArray, targetPosition);
-
-   if(currentCoordinate[0] !== targetPosition[0] || currentCoordinate[1] !== targetPosition[1]){
-     getPossibleCoordinates(currentCoordinate, targetPosition)
-  };
-  // if(currentCoordinate[0] === targetPosition[0] && currentCoordinate[1] === targetPosition[1]){
-  //   alert("done")
-  // }
-}
-
-  getPossibleCoordinates([0,0], [2, 6]);
-
-
+     
+    } 
+  return counter;
  
-
+}
+console.log(getPossibleCoordinates([1,1], [7,7]));
+ 
   return (
     <div>
       <h1>{counter}</h1>
+    
     </div>
   );
 }
